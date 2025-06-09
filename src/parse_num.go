58401df81ef51003a34d1d65bfcce8e500327adb
kanishkaprism <kanishka.thakur@prism.training>
2025-06-09 16:41:17 +0100

    Split the parseNumToWords, into 2 functions (added helper func)

--- src/parse_num.go
+++ src/parse_num.go
@@ -27,9 +27,60 @@ var teensPlace = map[int]string{
 	19: "nineteen",
 }
 
+// Function for converting chunks of the number
+func convertChunkToWords(num int) string {
+	if num == 0 {
+		return ""
+	}
+
+	if num < 10 {
+		return onesPlace[num]
+	}
+
+	if num < 20 {
+		return teensPlace[num]
+	}
+
+	var wordBuilder strings.Builder
+
+	hundreds := num / 100
+	remainder := num % 100
+
+	if hundreds > 0 {
+		wordBuilder.WriteString(onesPlace[hundreds])
+		wordBuilder.WriteString(" hundred")
+		if remainder > 0 {
+			wordBuilder.WriteString(" and ")
+		}
+	}
+
+	if remainder >= 10 && remainder <= 19 {
+		wordBuilder.WriteString(teensPlace[remainder])
+		return wordBuilder.String()
+	}
+
+	tens := remainder / 10
+	ones := remainder % 10
+
+	if tens > 0 {
+		wordBuilder.WriteString(tensPlace[tens])
+		if ones > 0 {
+			wordBuilder.WriteString("-")
+			wordBuilder.WriteString(onesPlace[ones])
+			return wordBuilder.String()
+		}
+	}
+
+	if ones > 0 {
+		wordBuilder.WriteString(onesPlace[ones])
+	}
+
+	return wordBuilder.String()
+}
+
 func parseNumToWords(num int) (string, error) {
 
-	if num > 1000 || num < 0 {
+	if num > 100000 || num < 0 {
 		return "", fmt.Errorf("input out of range: Please provide an integer from 0 to 99")
 	}
 
@@ -37,48 +88,29 @@ func parseNumToWords(num int) (string, error) {
 		return "zero", nil
 	}
 
-	if num == 1000 {
-		return "one thousand", nil
+	if num == 100000 {
+		return "one hundred thousand", nil
 	}
+
+	thousands := num / 1000
+	remainder := num % 1000
 
 	var wordBuilder strings.Builder
 
-	hundreds := num / 100
-
-	if hundreds > 0 {
-		wordBuilder.WriteString(onesPlace[hundreds])
-		wordBuilder.WriteString(" hundred")
+	if thousands > 0 {
+		wordBuilder.WriteString(convertChunkToWords(thousands))
+		wordBuilder.WriteString(" thousand")
 	}
 
-	remainder := num % 100
-
 	if remainder > 0 {
-
-		if wordBuilder.Len() > 0 {
-			wordBuilder.WriteString(" and ")
-		}
-
-		if remainder >= 10 && remainder <= 19 {
-			wordBuilder.WriteString(teensPlace[remainder])
-		} else {
-
-			tens := remainder / 10
-
-			if tens > 0 {
-				wordBuilder.WriteString(tensPlace[tens])
-			}
-
-			ones := remainder % 10
-
-			if wordBuilder.Len() > 0 && tens > 0 && ones > 0 {
-				wordBuilder.WriteString("-")
-			}
-
-			if ones > 0 {
-				wordBuilder.WriteString(onesPlace[ones])
+		if thousands > 0 {
+			if remainder < 100 {
+				wordBuilder.WriteString(" and ")
+			} else {
+				wordBuilder.WriteString(", ")
 			}
 		}
-
+		wordBuilder.WriteString(convertChunkToWords(remainder))
 	}
 
 	return wordBuilder.String(), nil
